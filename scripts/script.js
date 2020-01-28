9a1fa6abaf4ce2b781d4a203ad65e6c430eccf12
Dincius <diana.meksriunaite@gmail.com>
2020-01-29 00:39:46 +0200

    Second try

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,19 @@ async function galleryLoad() {
   const response = await fetch('https://backend.daviva.lt/API/InformacijaTestui');
   const images = await response.json();
 
-  let image1 = document.createElement('div');
+  
   document.getElementById("info").innerHTML = `<p id="car-brand">` + images.marke + `</p><p id="car-model">` + images.modelis + `</p><p id="year">` + images.metai + `</p><p id="price">` + images.kaina + " &#8364; </p>";
-
-
+  let image1 = document.createElement('div');
+ 
   for (let i = 0; i < 4; i++){
-  image1.classList.add('item');
+   image1.classList.add('item');
+ //  if(i = 1){
+	// 	image1.classList.add(' active');
+	// }
   image1.innerHTML = `<img src="${images.nuotraukos[i]}" alt="${images.marke}"/>`;
   document.getElementById("img-container").appendChild(image1);
 	}
+
 }
 
  function addNewCar() {
